3762cd58764b2d86084c0aeb91ea0ad7ec8d6589
Anton Shkumat <nous927@gmail.com>
2026-05-28 16:04:38 +0200

    fix pylint issues

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
+"""Calculator utility functions."""
+
+
 def add(a: int, b: int) -> int:
+    """Return sum of two numbers."""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Return difference of two numbers."""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Return multiplication of two numbers."""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Return division of two numbers."""
     return a / b
